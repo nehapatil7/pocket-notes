aa63f95b39c15e4c1677a3368d6be4165df02abb
nehapatil7 <nehapatil0507>
2023-12-15 20:21:26 +0530

    added

---
+++ src/components/CreateGroup.jsx
@@ -0,0 +1,79 @@
+import './CreateGroup.css'
+import { useState } from 'react'
+
+const groupColors =[
+    "#B38BFA",
+    "#FF79F2",
+    "#43E6FC",
+    "#F19576",
+    "#0047FF",
+    "#6691FF",
+]
+
+const CreateGroup = ({closeModal, dataStored}) => {
+
+    const [groupName , setGroupName] = useState ()
+    const [groupColor , setGroupColor] = useState ()
+
+    
+
+    const onColorClick = (color) => setGroupColor(color);
+
+    const handleCreateButtonClick = () => {
+        const newGroup = {
+            id: Date.now(),
+            group: { groupName, groupData: [], groupColor },
+            
+          };
+      
+         dataStored(newGroup);
+        }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!groupName.trim() || !groupColor) {
+            alert("Please enter a group name.");
+          return; 
+        }
+        handleCreateButtonClick(); 
+        
+         
+      };
+      
+
+    return (
+      <>
+      <div className='container' onClick={closeModal}></div>
+        <div className="createGroup">
+          <h2>Crete New Group</h2>
+          <div className="name">
+            <form>
+            <label htmlFor="GroupName">Group Name</label>
+            <input type="text" id="groupname" name="" placeholder='Enter group name' value={groupName} onChange={(e) => setGroupName(e.target.value)}></input>
+            </form>
+          </div>
+
+          <div className="color">
+            <label for="GroupName">Group Color</label>
+            <div className="colorsImg">
+
+            {groupColors.map((color) => (
+              <div
+                key={color}
+                className="colors"
+                style={{ backgroundColor: color }}
+                onClick={() => onColorClick(color)}
+                value={groupColor}
+                onChange={(e) => setGroupColor(e.target.value)}
+              />
+            ))}
+            </div>
+          </div>
+
+          <button className='create-btn' onChange={handleSubmit} onClick={closeModal}>Create</button>
+        </div>
+      </>
+    );
+  };
+
+  export default CreateGroup;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react'
+import './Home.css'
+import create from '../../assets/+.png'
+import noNotes from '../../assets/noNotes.png'
+import CreateGroup from '../../components/CreateGroup.jsx'
+import lock from '../../assets/lock.png'
+
+
+function Home() {
+  const [noteGroups, setNoteGroups] = useState()
+  
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() =>{
+    localStorage.setItem('items', JSON.stringify(noteGroups))
+  }, [noteGroups])
+
+  return (
+    <div className='home'>
+        <div className="sidebar">
+            <p>Pocket Notes</p>
+            <div className="groupNames">
+
+            <button className="add-btn" onClick={toggleModal}>
+                <img src={create} alt="create button" />
+            </button>
+            {isOpen && <CreateGroup  closeModal={toggleModal}/>}
+            </div>
+            
+        </div>
+
+        <div className="notesContainer">
+         <div className="title"></div>
+            <div className="notes">
+              
+
+            
+            {/*<img src={noNotes} alt="" />
+            <h1>Pocket Notes</h1>
+            <p>Send and receive messages without keeping your phone online.
+Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+<div className='rights'>
+              <img src={lock} alt="" />
+            end-to-end encrypted
+  </div>*/}
+                
+            </div>
+            <div className="textarea">
+
+            </div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
